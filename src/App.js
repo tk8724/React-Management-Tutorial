bc3f5484415b796e3b47b14ba922607861003447
tk8724 <tonykim8724@naver.com>
2020-11-15 20:41:41 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김효찬',
+    'birthday': '870424',
+    'gender': '남자',
+    'job': '개발자'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '장미나',
+    'birthday': '570317',
+    'gender': '여자',
+    'job': '주부'
+  }
+]
 
 class  App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
